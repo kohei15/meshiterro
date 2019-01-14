94dedbed1bc4ec1b9d9bc56cf1eed60a3f2664a3
木下晃兵 <kinoshitakohei@MacBook-Air-2.local>
2019-01-14 15:46:31 +0900

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'dca2396eb14d586854a5e01286a9b551f0832da257d2b30a87ff9f77d71f4fba83238d9e45c1593ace78cf6f90d67edf8c8a71703226561c72cac4efc195908f'
